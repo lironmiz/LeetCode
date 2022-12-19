f2626e4aed99a94f34ab969c5717788d7de1ccb5
Liron-Mizrahi <91504420+lironmiz@users.noreply.github.com>
2022-12-19 10:33:25 +0200

    fix doc

--- isSymmetric.java
+++ isSymmetric.java
@@ -11,7 +11,7 @@ public class LeetCode
    public boolean isSymmetric(TreeNode root) 
    {
       return isMirror(root, root);      
-   }
+   }// end of method isSymmetric 
    public boolean isMirror(TreeNode tree1, TreeNode tree2)
    {
         if(tree1 == null && tree2 == null)
@@ -23,5 +23,5 @@ public class LeetCode
             return false;
         }
         return (tree1.val == tree2.val) && isMirror(tree1.left, tree2.right) && isMirror(tree1.right, tree2.left);
-   }
+   }// end of method isMirror
 }// end of class LeetCode
