fe2d55547e7930182038b3f869fe1f8e11f3c714
Liron-Mizrahi <91504420+lironmiz@users.noreply.github.com>
2023-01-15 15:19:27 +0200

    Create MajorityElement.java

---
+++ MajorityElement.java
@@ -0,0 +1,39 @@
+import java.util.HashMap;
+
+/**
+* Class LeetCode.java
+* A class with solutions to leet code problems 
+*/
+public class LeetCode
+{
+   /**
+    * https://leetcode.com/problems/palindrome-number/
+    * Easy
+    * problem 169 Majority Element
+    * Given an array nums of size n, return the majority element.
+    * The majority element is the element that appears more than ⌊n / 2⌋ times.
+    * You may assume that the majority element always exists in the array.
+    * Array, Hash Table Divide and Conquer, Sorting, Counting 
+    */
+    public int majorityElement(int[] nums)
+    {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int num : nums)
+        {
+            if(!map.containsKey(num))
+            {
+                map.put(num, 1);
+            }
+            else
+            {
+                map.put(num, map.get(num) + 1);
+            }
+            if(map.get(num) > nums.length / 2)
+            {
+                return num;
+            }
+        }
+        return 0;
+    }
+}// end of class LeetCode
+    
