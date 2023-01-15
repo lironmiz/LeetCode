964025df5735758e4af0c2d9d9da4b94b66c5584
Liron-Mizrahi <91504420+lironmiz@users.noreply.github.com>
2023-01-15 15:24:30 +0200

    Update ExcelSheetColumnTitle.java

--- ExcelSheetColumnTitle.java
+++ ExcelSheetColumnTitle.java
@@ -1,5 +1,16 @@
-class LeetCode 
+/**
+* Class LeetCode.java
+* A class with solutions to leet code problems 
+*/
+public class LeetCode
 {
+    /**
+    * https://leetcode.com/problems/excel-sheet-column-title/
+    * Easy
+    * problem 168 Excel Sheet column Title 
+    * Given an integer columnNumber, return its corresponding column title as it appears in an Excel sheet.
+    * Math, String
+    */
     public String convertToTitle(int columnNumber)
     {
         String str = "";
@@ -10,7 +21,6 @@ class LeetCode
             str = (char) (columnNumber%26 + 'A') + str;
             columnNumber = columnNumber / 26;
         }
-
         return str;
     }// end of method convertToTitle
 }// end of class LeetCode
